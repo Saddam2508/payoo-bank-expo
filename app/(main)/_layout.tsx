f84e9d6ef2f732ca66bb1ff013bae133cde78e34
Saddam <eyarinsultana@gmail.com>
2026-03-29 07:51:11 +0600

    expo update run...

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,13 +1,10 @@
 import { Stack } from "expo-router";
-import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function MainLayout() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
-        <Stack screenOptions={{ headerShown: false }} />
-      </View>
+      <Stack screenOptions={{ headerShown: false }} />
     </SafeAreaView>
   );
 }
