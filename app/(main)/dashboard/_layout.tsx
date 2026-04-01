fdf0968ee6a7f807021ef7bbb2c9b3313ba3c275
Saddam <eyarinsultana@gmail.com>
2026-04-01 08:40:19 +0600

    dashboard update inprogress

--- app/(main)/dashboard/_layout.tsx
+++ app/(main)/dashboard/_layout.tsx
@@ -1,11 +1,64 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function DashboardLayout() {
+  const router = useRouter();
+
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Dashboard" }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "",
+          headerLeft: () => (
+            <View style={styles.logoContainer}>
+              <Image
+                style={styles.dashboardLogo}
+                source={require("../../../assets/images/logo.png")}
+              />
+              <View>
+                <Text style={styles.amountText}>$50000</Text>
+                <Text>Available Balance</Text>
+              </View>
+            </View>
+          ),
+          headerRight: () => (
+            <Pressable
+              style={styles.logoutButton}
+              onPress={() => router.replace("/(auth)/login")}
+            >
+              <Text style={styles.logoutText}>Logout</Text>
+            </Pressable>
+          ),
+        }}
+      />
       <Stack.Screen name="analytics" options={{ title: "Analytics" }} />
       <Stack.Screen name="reports" options={{ title: "Reports" }} />
     </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  logoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 16,
+    marginLeft: 5,
+    gap: 10,
+  },
+  dashboardLogo: {
+    width: 40,
+    height: 40,
+  },
+  amountText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  logoutButton: {
+    backgroundColor: "#ef4444",
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    borderRadius: 6,
+  },
+  logoutText: { color: "white", fontWeight: "bold" },
+});
--- app/(main)/dashboard/index.tsx
+++ app/(main)/dashboard/index.tsx
@@ -1,81 +1,68 @@
 import { useRouter } from "expo-router";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import menuJson from "../../../public/menu.json";
 
 type MenuItem = {
+  id: number;
   title: string;
-  route: "/dashboard/analytics" | "/dashboard/reports";
+  route: string;
+  icon: string;
 };
 
 export default function Dashboard() {
   const router = useRouter();
 
-  const menus: MenuItem[] = [
-    { title: "Analytics", route: "/dashboard/analytics" },
-    { title: "Reports", route: "/dashboard/reports" },
-  ];
-
   return (
     <View style={{ flex: 1, padding: 15 }}>
-      {/* Header */}
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: 20,
-        }}
-      >
-        {/* Logo */}
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Image
-            source={{ uri: "https://via.placeholder.com/40" }}
-            style={{ width: 40, height: 40, marginRight: 10 }}
-          />
-          <Text style={{ fontSize: 18, fontWeight: "bold" }}>My Admin</Text>
-        </View>
-
-        {/* Logout */}
-        <TouchableOpacity
-          onPress={() => router.replace("/(auth)/login")}
-          style={{
-            backgroundColor: "#ef4444",
-            paddingHorizontal: 12,
-            paddingVertical: 6,
-            borderRadius: 6,
-          }}
-        >
-          <Text style={{ color: "white" }}>Logout</Text>
-        </TouchableOpacity>
+      {/* welcome message */}
+      <View>
+        <Text style={styles.welcome}>Welcome to Payoo</Text>
+        <Text style={styles.enjoy}>
+          Enjoy easy and convenient financial services with us. Cashout charge
+          is low and
+        </Text>
       </View>
 
       {/* Grid */}
-      <View
-        style={{
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "space-between",
-        }}
-      >
-        {menus.map((item, index) => (
-          <TouchableOpacity
-            key={index}
-            onPress={() => router.push(item.route)}
-            activeOpacity={0.7}
-            style={{
-              width: "48%",
-              backgroundColor: "#1e293b",
-              padding: 20,
-              borderRadius: 10,
-              marginBottom: 15,
-              alignItems: "center",
-            }}
+      <View style={styles.grid}>
+        {(menuJson as MenuItem[]).map((item) => (
+          <Pressable
+            key={item.id}
+            onPress={() => router.push(item.route as any)}
+            style={styles.dashboardIcon}
           >
             <Text style={{ fontSize: 30, marginBottom: 10 }}>📊</Text>
 
             <Text style={{ color: "white" }}>{item.title}</Text>
-          </TouchableOpacity>
+          </Pressable>
         ))}
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  welcome: { fontSize: 24, fontWeight: "bold", marginBottom: 20 },
+  enjoy: { fontSize: 16, marginBottom: 20, color: "#334155" },
+
+  grid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    gap: 5,
+  },
+
+  dashboardIcon: {
+    width: "30%",
+    backgroundColor: "#faf6f6",
+    padding: 20,
+    borderRadius: 20,
+    marginBottom: 15,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
